42b96bcfce60ba782fcf6a8df1c32072e7dfa233
ryou-yattung <92421273+ryou-yattung@users.noreply.github.com>
2022-01-15 21:52:22 +0900

    0115

---
+++ src/components/Hero.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Hero() {
+  return (
+    <div className="bg-white h-screen flex flex-col justify-center items-center">
+      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
+        aaaaaaaa
+      </h1>
+      <Link
+        to="/"
+        className="py-6 px-14 bg-yellow-500 rounded-full text-3xl 
+        hover:bg-green-600 transition duration-300 ease-in-out flex items-center animate-bounce"
+      >
+        Hello
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 ml-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
+          />
+        </svg>
+      </Link>
+    </div>
+  );
+}
+
+export default Hero;
